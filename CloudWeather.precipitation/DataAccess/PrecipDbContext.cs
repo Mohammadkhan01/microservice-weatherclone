31f3c78289bcdf417deb27f96eb3ac342e5e98a4
Mohammad Khan <Mosiulhaque@gmail.com>
2022-07-06 08:08:19 +1000

    add data access

---
+++ CloudWeather.precipitation/DataAccess/PrecipDbContext.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CloudWeather.precipitation.DataAccess
+{
+    public class PrecipDbContext:DbContext
+    {
+        public PrecipDbContext()
+        {
+
+        }
+        public PrecipDbContext(DbContextOptions opts) : base(opts) 
+        {
+
+        }
+        public DbSet<Precipitation> Precipitations { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            SnakeCaseIdentityTableNames(modelBuilder);
+        }
+
+        private void SnakeCaseIdentityTableNames(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Precipitation>(b => {b.ToTable("precipitation"); });
+        }
+    }
+}
